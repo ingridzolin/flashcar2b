ecb571c1a44a8e96c7e7d6816245fb8698776591
ingridzolin <ingrid.zolin@escola.pr.gov.br>
2025-06-05 13:54:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    ' Monte Everest'
+)
+
+criaCartao(
+    'Biologia',
+    ' Quais são os 5 reinos em que os seres vivos são classificados?',
+    'Animalia, Plantae, Fungi, Protista e Monera. '
+)
+
+criaCartao(
+    'Sociologia',
+    'Quem foi o pai da sociologia?',
+    ' Auguste Comte'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amor em Inglês?',
+    'Amor em ingles é love'
+)
